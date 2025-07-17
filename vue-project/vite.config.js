24a7905be7c289f091a8e5f57520eb40373067c8
albarstring <rizqizulmiaztaalbar1@gmail.com>
2025-07-17 17:30:53 +0700

    Deploy to GitLab Pages

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,4 +1,7 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url' // ← Tambahkan ini
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
